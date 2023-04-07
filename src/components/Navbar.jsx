779404f0ddcfe8525b58d3f1b651374795f61248
Vinh7252 <ngducvinh72@gmail.com>
2023-04-07 15:05:31 +0700

    khung-navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+export default function NavBar(){
+    return(
+        <div>
+            {/*Container */}
+            <div>
+                {/* Navbar Container */}
+                <div>
+                    {/* Icon web */}
+                    <div>
+
+                    </div>
+                    {/* catelogy */}
+                    <div>
+
+
+                    </div>
+                    {/* Icon cart */}
+                    <div>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
