b2b21ab913648120abeb400b5f53a6149414de6a
phuong-nguyen-24 <phuongnguyen241096@gmail.com>
2023-04-09 20:04:32 +0700

    Update Footer,Location,Newsletter

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,7 +1,25 @@
-export default function Location () {
-    return (
-        <section>
-            
-        </section>
-    )
+export default function Location() {
+  return (
+    <section>
+      {/* Container */}
+      <div className="my-12 mx-4">
+        {/* Location-container */}
+        <div className="relative">
+          <div className="max-w-7xl h-60 lg:h-auto">
+            <img
+              className="w-full h-full object-cover"
+              src="https://cdn.shopify.com/s/files/1/0691/4641/files/poketo_d32cc356-f79d-464c-8f1a-e0963921d82b_1380x400_crop_center.jpg?v=1613532931"
+              alt=""
+            />
+          </div>
+          <div className="text-center mt-5 lg:absolute lg:left-2/4 lg:top-2/4 lg:-translate-x-2/4 lg:-translate-y-2/4 lg:border lg:p-6 bg-white">
+            <h4 className="pb-5 text-xs font-medium text-gray-600">VICTORIA LOCATION</h4>
+            <p className="pb-1.5 text-xs font-extralight">1221 Broad Street</p>
+            <p className="pb-1.5 text-xs font-extralight">Tuesday to Sunday: 10 - 5</p>
+            <p className="text-xs font-extralight">+1 (250) 555-5555</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
