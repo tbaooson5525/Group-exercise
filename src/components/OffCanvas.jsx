95d35c70389fc4c73d505fb46f349f44368ded1f
Vinh7252 <ngducvinh72@gmail.com>
2023-04-10 00:55:27 +0700

    Update OffCanvas.jsx

--- src/components/OffCanvas.jsx
+++ src/components/OffCanvas.jsx
@@ -4,8 +4,8 @@ export default function Offcanvas({ chidren }) {
       <button>
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width={16}
-          height={16}
+          width={20}
+          height={20}
           fill="currentColor"
           className="bi bi-list"
           viewBox="0 0 16 16"
