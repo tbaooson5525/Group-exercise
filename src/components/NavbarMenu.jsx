0e63f75857b34d7b14a15ad071ed603cb92530e2
Vinh7252 <ngducvinh72@gmail.com>
2023-04-10 17:08:04 +0700

    navbar menu

--- src/components/NavbarMenu.jsx
+++ src/components/NavbarMenu.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 const NavbarMenu = () => {
   return (
-    <div>
+    <div className="absoabsolute">
         <ul>
-            <li><a href=""></a></li>
+            <li><a href="">New Arrivals</a></li>
+            <li><a href="">Stationery</a></li>
+            <li><a href="">Home Goods</a></li>
+            <li><a href="">Sale</a></li>
+            <li><a href="">About Us</a></li>
+            <li><a href="">Blog</a></li>
         </ul>
     </div>
   )
--- src/components/OffCanvas.jsx
+++ src/components/OffCanvas.jsx
@@ -1,3 +1,5 @@
+import NavbarMenu from "./NavbarMenu";
+
 export default function Offcanvas() {
   return (
     <div>
@@ -17,10 +19,40 @@ export default function Offcanvas() {
         </svg>
       </button>
       <div>
-
+        <svg
+          width="20px"
+          height="20px"
+          viewBox="0 -0.5 21 21"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+        >
+          <title>close [#1511]</title>
+          <desc>Created with Sketch.</desc>
+          <defs />
+          <g
+            id="Page-1"
+            stroke="none"
+            strokeWidth={1}
+            fill="none"
+            fillRule="evenodd"
+          >
+            <g
+              id="Dribbble-Light-Preview"
+              transform="translate(-419.000000, -240.000000)"
+              fill="#000000"
+            >
+              <g id="icons" transform="translate(56.000000, 160.000000)">
+                <polygon
+                  id="close-[#1511]"
+                  points="375.0183 90 384 98.554 382.48065 100 373.5 91.446 364.5183 100 363 98.554 371.98065 90 363 81.446 364.5183 80 373.5 88.554 382.48065 80 384 81.446"
+                />
+              </g>
+            </g>
+          </g>
+        </svg>
+        <NavbarMenu />
       </div>
-
-      
     </div>
   );
 }
