a230892f258265670f987a6bcb4b3d3537e74d1d
Bao Son <tbaoson5525@gmail.com>
2023-04-10 13:58:51 +0700

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'custom': ['Madera', 'sans-serif'],
+      },
+    }
   },
   plugins: [],
 }
